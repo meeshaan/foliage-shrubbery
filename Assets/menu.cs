c12eb43c4270e2d66e5f11c0a8fb58b62c2bab10
isaac109 <isaac109@mail.chapman.edu>
2015-10-06 16:09:11 -0700

    Here is what I have so far
    Do not use the non oculus version. I was trying to make something work
    and the script doesn't want to work on it, but the oculus version in the
    exe works fine.

---
+++ Assets/menu.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class menu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnGUI(){
+		if(GUI.Button(new Rect(Screen.width/2-100,Screen.height/2-100,200,200),"Oculus"))
+	    {
+			Application.LoadLevel(1);
+		}
+		if(GUI.Button(new Rect(Screen.width/2-100,Screen.height/2+100,200,200),"No Oculus"))
+		{
+			Application.LoadLevel(2);
+		}
+	}
+}
---
+++ Assets/movementScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movementScript : MonoBehaviour {
+
+	public GameObject Player;
+	public Vector3 movementSpeed;
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (Input.GetMouseButton (1)) {
+			Player.transform.Translate (Vector3.forward * speed);
+		} else {
+			Player.transform.Translate(Vector3.zero);
+		}
+	}
+}

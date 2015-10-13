f20050504f85bab1dd5917f55db2127c920f3844
isaac109 <isaac109@mail.chapman.edu>
2015-10-13 14:49:31 -0700

    Can Now click cubes to make them YELLOW!

---
+++ Assets/cast.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cast : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			RaycastHit hit;
+			Vector3 fwd = transform.TransformDirection (Vector3.forward);
+			if (Physics.Raycast (this.transform.position, fwd, out hit, 10)) {
+				Debug.Log(hit.rigidbody.gameObject.tag.ToString());
+				if (hit.rigidbody.gameObject.tag == "plots") {
+					hit.collider.gameObject.GetComponent<plot>().change();
+
+				}
+			}
+		}
+
+	}
+}
---
+++ Assets/rayCast.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		RaycastHit hit;
+		Vector3 fwd = transform.TransformDirection (Vector3.forward);
+		if (Physics.Raycast (this.transform.position, fwd, out hit, 10)) {
+			if(hit.rigidbody.gameObject.tag == "plots")
+			{
+				Debug.Log("found a plot");
+			}
+		}
+
+	}
+}
